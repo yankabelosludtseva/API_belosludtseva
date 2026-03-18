2af6233e365333ad3782e147c80a144fac2102fb
yankabelosludtseva <nblsldcv@gmail.com>
2026-03-18 17:36:47 +0500

    Создание модели и контекста пользователя. Белослудцева

---
+++ API_belosludtseva/Context/UsersContext.cs
@@ -0,0 +1,35 @@
+﻿using API_belosludtseva.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_belosludtseva.Context
+{
+    public class UsersContext: DbContext
+    {
+        /// <summary>
+        /// Данные из базы данных
+        /// </summary>
+        public DbSet<Users> Users { get; set; }
+
+        /// <summary>
+        /// Конструктор контекста
+        /// </summary>
+        public UsersContext()
+        {
+            // Проверяем создана ли база данных
+            Database.EnsureCreated();
+            Users.Load();
+        }
+
+        /// <summary>
+        /// Переопределяем метод конфигурации
+        /// </summary>
+        /// <param name="optionsBuilder">Построитель параметров контекста</param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            // подключаемся к серверу MySQL, со следующими настройками
+            optionsBuilder.UseMySql("server=192.168.0.111;" +
+                                  "uid=root;" +
+                                  "pwd=root" +
+                                  "database=TaskManager",
+            new MySqlServerVersion(new Version(8, 0, 11)));
+    }
+}
---
+++ API_belosludtseva/Model/Users.cs
@@ -0,0 +1,20 @@
+﻿namespace API_belosludtseva.Model
+{
+    public class Users
+    {
+        /// <summary>
+        /// Код пользователя
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Пароль пользователя
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
