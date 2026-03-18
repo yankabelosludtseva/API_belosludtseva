c192f330812502a5431168eb33101617a7fcb130
yankabelosludtseva <nblsldcv@gmail.com>
2026-03-18 16:26:30 +0500

    Разработанные модели данных. Белослудцева

---
+++ API_belosludtseva/Model/Task.cs
@@ -0,0 +1,26 @@
+﻿namespace API_belosludtseva.Model
+{
+    public class Task
+    {
+        /// <summary>
+        /// Задачи
+        /// </summary>
+        public class Tasks
+        {
+            /// <summary>
+            /// Код
+            /// </summary>
+            public int Id { get; set; }
+            /// <summary> Наименование
+            public string Name { get; set; }
+            /// <summary> Приоритет
+            public string Priority { get; set; }
+            /// <summary> Дата выполнения
+            public DateTime DateTimeExecute { get; set; }
+            /// <summary> Комментарий
+            public string Comment { get; set; }
+            /// <summary> Выполнено
+            public bool Done { get; set; }
+        }
+    }
+}
