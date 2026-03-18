ce2d547d8d3b891ddb463b230c65f7af44167473
yankabelosludtseva <nblsldcv@gmail.com>
2026-03-18 17:01:02 +0500

    хз работает или нет, но должно

--- API_belosludtseva/Controllers/TasksController.cs
+++ API_belosludtseva/Controllers/TasksController.cs
@@ -1,6 +1,9 @@
 ﻿using API_belosludtseva.Context;
 using API_belosludtseva.Model;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace API_belosludtseva.Controllers
 {
@@ -32,6 +35,32 @@ namespace API_belosludtseva.Controllers
         }
 
         /// <summary>
+        /// Получение задачи по ID (альтернативный маршрут)
+        /// </summary>
+        /// <remarks>Данный метод получает задачу по ID из маршрута</remarks>
+        /// <param name="id">Идентификатор задачи</param>
+        /// <response code="200">Задача успешно получена</response>
+        /// <response code="404">Задача не найдена</response>
+        /// <response code="500">При выполнении запроса возникли ошибки</response>
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                // Получаем задачу по коду
+                Tasks? task = new TaskContext().Tasks.Where(x => x.Id == id).FirstOrDefault();
+                if (task == null)
+                    return NotFound();
+
+                return Json(task); // возвращаем ответ в виде Json
+            }
+            catch (Exception)
+            {
+                return StatusCode(500); // если возникли неполадки, выдаём 500 ошибку (ошибку сервера)
+            }
+        }
+
+        /// <summary>
         /// Получение задачи
         /// </summary>
         /// <remarks>Данный метод получает задачу, находящуюся в базе данных</remarks>
@@ -50,6 +79,31 @@ namespace API_belosludtseva.Controllers
                     return NotFound();
 
                 return Json(task); // возвращаем ответ в виде Json
+            }
+            catch (Exception)
+            {
+                return StatusCode(500); // если возникли неполадки, выдаём 500 ошибку (ошибку сервера)
+            }
+        }
+
+        /// <summary>
+        /// Поиск задач по наименованию
+        /// </summary>
+        /// <remarks>Данный метод выполняет поиск задач по части наименования</remarks>
+        /// <param name="search">Строка поиска</param>
+        /// <response code="200">Список найденных задач</response>
+        /// <response code="500">При выполнении запроса возникли ошибки</response>
+        [HttpGet("search")]
+        public IActionResult Search([FromQuery] string search)
+        {
+            try
+            {
+                // Выполняем поиск задач по наименованию
+                IEnumerable<Tasks> tasks = new TaskContext().Tasks
+                    .Where(x => x.Name.Contains(search))
+                    .ToList();
+
+                return Json(tasks); // возвращаем ответ в виде JSON
             }
             catch (Exception)
             {
--- API_belosludtseva/Program.cs
+++ API_belosludtseva/Program.cs
@@ -1,7 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+// Удаляем builder.Services.AddRazorPages(); - эта строка вызывает ошибки
+builder.Services.AddControllers();
 builder.Services.AddMvc(option => option.EnableEndpointRouting = true);
 builder.Services.AddSwaggerGen(option => {
     option.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
@@ -9,7 +10,7 @@ builder.Services.AddSwaggerGen(option => {
         Version = "v1",
         Title = "Пробная версия"
     });
-    string PathFile = Path.Combine(System.AppContext.BaseDirectory, "WebApplication2.xml");
+    string PathFile = Path.Combine(System.AppContext.BaseDirectory, "API_belosludtseva.xml");
     option.IncludeXmlComments(PathFile);
 });
 
@@ -25,6 +26,5 @@ app.UseEndpoints(endpoints =>
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Проба версия");
-
 });
 app.Run();
